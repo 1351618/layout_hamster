89dcce83c157609cf1e92342fabf7d0e97de6620
1351618 <1351618@gmail.com>
2024-11-08 16:36:44 +0200

    fix r1

--- src/js/popup.js
+++ src/js/popup.js
@@ -3,6 +3,16 @@ const popupDiv = document.querySelector(".popup");
 const addInformBtn = document.getElementById("addInformBtn");
 const popupNotActiveBtn = document.getElementById("popupNotActive");
 
+const addInformData = ` 
+    <img src="./src/images/notification.svg" alt="" />
+    <p>
+        Ваш хомяк работает ровно 24 часа, после чего он уснёт. Чтобы
+        разбудить его и включить в работу на 24 часа, надо нажать на него.
+        <br/>
+        <br/>
+        Вы можете нажимать по хомяку в любое время.Счётчик обновится.
+    </p>`;
+
 function activePopupDiv() {
   popupDiv.classList.toggle("active");
 }
